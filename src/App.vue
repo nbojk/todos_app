f0223a0bb5362af7b54e2a34a59843e53939e551
Neven Bojkovic <nev.enbo@gmail.com>
2020-05-31 22:50:44 +0200

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To Do List</h1>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -23,6 +23,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body{
+  margin:0px;
+  padding:0px;
+  box-sizing: border-box;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li>
+        <input type="checkbox" v-model="newTask.checked" name="newCheckbox" id="newCheckbox"> 
+        <input type="text" v-bind:placeholder="placeholderValues.question" v-on:keyup.enter="addNewTask"  v-model="newTask.description" name="newTask" id="newTask">
+        <button id="clearAllBtn" v-on:click="removeAllTasks">Clear List</button>
+      </li>
+      <li v-for="(item, index) in tasks" v-bind:key="index"> 
+        <input type="checkbox" v-model="item.checked" name="checkbox" id="checkbox">
+        <span>{{item.description}}</span>
+        <button id="removeTaskBtn" v-on:click="removeTask(index)">&#10060;</button>
+      </li>
     </ul>
   </div>
 </template>
@@ -33,26 +18,127 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function(){
+    return {
+      tasks:[{
+          description:'This is an example task. Delete or add your own',
+          checked: false
+        },
+        {
+          description:'This is an example task. Delete or add your own',
+          checked: true
+        },
+        {
+          description:'This is an example task. Delete or add your own',
+          checked: false
+        },
+        {
+          description:'This is an example task. Delete or add your own',
+          checked: true
+        }
+      ],
+      newTask:[{
+        description:'',
+        checked:''
+      }],
+      placeholderValues:{
+        question:'What do you need to do?'
+      }
+    }
+  },
+  methods:{
+    addNewTask: function(){
+      if(this.newTask.checked){
+        console.log('checked');
+        this.newTask.checked = true;
+      }   else {
+        console.log('Not checked');
+        this.newTask.checked = false ; 
+      }
+      this.tasks.unshift({
+        description: this.newTask.description,
+        checked: this.newTask.checked
+      });
+      this.newTask.description = '',
+      this.newTask.checked = false 
+    },
+    removeTask: function(taskIndex){
+      this.tasks.splice(taskIndex, 1);
+      
+    },
+    removeAllTasks: function(){
+        this.tasks.splice(0,this.tasks.length);
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello {
+  border: 1px solid #a5a2a2;
+  margin:15px 60px 0 60px;
+  ul{
+    overflow: hidden;
+    margin:0px;
+    padding:0;
+    li{
+      list-style-type: none;
+      display:flex;
+      justify-content: space-around;
+      align-items: center;
+      background-color: #3464a3;
+      padding:15px 0;
+      margin:0 0 1px 0;
+      &:first-child{
+        background-color:#E7E8EB;
+        margin:0px;
+        z-index:1;
+        position: relative;
+        #newTask{
+          border:1px solid #333;
+          width:65%;
+          padding:15px 10px 15px 10px;
+          font-size:14px;
+        }
+        #clearAllBtn {
+          padding:12px 20px;
+          font-size: 18px;
+          color:#333;
+          background-color:#fff;
+          border:1px solid #a5a2a2;
+        }
+      }
+      &:last-child{
+        margin:0px;
+      }
+      #removeTaskBtn {
+        opacity:0%;
+        transition:0.3s opacity;
+        margin:10px 10px 10px 74px;
+        background-color:#3564A4;
+        border:none;
+        font-size:20px;
+        &:hover{
+            cursor:pointer;
+          }
+      }
+      &:hover #removeTaskBtn{
+          opacity:100%;
+        }
+      #newCheckbox, #checkbox {
+        width:20px;
+        height:20px;
+      }
+      span {
+        width:65%;
+        padding:0 10px;
+        font-size:14px;
+        color:#DDE2EC;
+        text-align: left;
+        font-size:25px;
+      }
+    }
+  }
 }
 </style>
